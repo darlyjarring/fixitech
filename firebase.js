ad3ee1f7ea2adb977355012a08a55801e211bc96
jlgaibor <josejarrin3@gmail.com>
2025-07-20 23:55:22 -0500

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// Configura tus datos reales de Firebase aquí
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_DOMINIO.firebaseapp.com",
+  projectId: "TU_PROJECT_ID",
+  storageBucket: "TU_BUCKET.appspot.com",
+  messagingSenderId: "ID_MENSAJES",
+  appId: "TU_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
